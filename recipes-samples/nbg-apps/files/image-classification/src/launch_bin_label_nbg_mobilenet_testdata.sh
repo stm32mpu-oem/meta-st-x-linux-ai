fa8c8c61cf7eaa93a73d149d9a36418c0c9a6079
Alexis BRISSON <alexis.brisson@st.com>
2023-05-15 16:33:33 +0200

    nbg-apps: add image classification app
    First release of the NBG image classification application
    
    Signed-off-by: Alexis BRISSON <alexis.brisson@st.com>

---
+++ recipes-samples/nbg-apps/files/image-classification/src/launch_bin_label_nbg_mobilenet_testdata.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+weston_user=$(ps aux | grep '/usr/bin/weston '|grep -v 'grep'|awk '{print $1}')
+
+source /usr/local/demo-ai/computer-vision/nbg-image-classification/bin/resources/config_board.sh
+cmd="/usr/local/demo-ai/computer-vision/nbg-image-classification/bin/label_nbg_gst_gtk  -m /usr/local/demo-ai/computer-vision/nbg-image-classification/bin/mobilenet.nb -l /usr/local/demo-ai/computer-vision/models/mobilenet/labels.txt -i /usr/local/demo-ai/computer-vision/models/mobilenet/testdata/"
+
+if [ "$weston_user" != "root" ]; then
+	echo "user : "$weston_user
+	script -qc "su -l $weston_user -c '$cmd'"
+else
+	$cmd
+fi
