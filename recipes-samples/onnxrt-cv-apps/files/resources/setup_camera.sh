4460de779f2c85463c96b09a3f8ae2333d95ac08
Maxence GUILHIN <maxence.guilhin@st.com>
2023-07-26 14:53:33 +0200

    camera : adapt camera setup script for MP1x series
    Modify camera setup script to support all official camera for MP1x series
    
    Signed-off-by: Maxence GUILHIN <maxence.guilhin@st.com>

--- recipes-samples/onnxrt-cv-apps/files/resources/setup_camera.sh
+++ recipes-samples/onnxrt-cv-apps/files/resources/setup_camera.sh
@@ -7,7 +7,6 @@ CAMERA_WIDTH=640
 CAMERA_HEIGHT=480
 
 is_dcmipp_present() {
-    echo "is_dcmipp_present function"
     DCMIPP_SENSOR="NOTFOUND"
     # on disco board ov5640 camera can be present on csi connector
     for video in $(find /sys/class/video4linux -name "video*" -type l);
@@ -27,6 +26,11 @@ is_dcmipp_present() {
                     bridgesubdev=$(media-ctl -d $mediadev -p -e "$sensorsubdev" | grep ":0 \[ENABLED" | awk -F\" '{print $2}')
                     #interface is connected to input of postproc (":1 [ENABLED" with media-ctl -p)
                     interfacesubdev=$(media-ctl -d $mediadev -p -e "dcmipp_dump_postproc" | grep ":1 \[ENABLED" | awk -F\" '{print $2}')
+                    echo "media device: "$mediadev
+                    echo "video device: "$V4L_DEVICE
+                    echo "sensor    subdev: " $sensorsubdev
+                    echo "bridge    subdev: " $bridgesubdev
+                    echo "interface subdev: " $interfacesubdev
                     return
                 fi
             done
@@ -65,7 +69,7 @@ if [ "$DEVICE" != "" ]; then
 	    subdev_name=$(tr -d '\0' < $sub/name | awk '{print $1}')
             if [ "$subdev_name" = "gc2145" ] || [ "$subdev_name" = "ov5640" ]; then
                 DCMIPP_SENSOR=$subdev_name
-	        echo "DCMIPP_SENSOR="$DCMIPP_SENSOR
+	            echo "DCMIPP_SENSOR="$DCMIPP_SENSOR
                 V4L_DEVICE="$(basename $DEVICE)"
                 sensorsubdev=$(tr -d '\0' < $sub/name)
                 #bridge is connected to output of sensor (":0 [ENABLED" with media-ctl -p)
@@ -74,24 +78,31 @@ if [ "$DEVICE" != "" ]; then
                 interfacesubdev=$(media-ctl -d $mediadev -p -e "dcmipp_dump_postproc" | grep ":1 \[ENABLED" | awk -F\" '{print $2}')
 	   fi
         done
-    fi
+	else
+		echo "camera specified not valid ... try to find another camera"
+		is_dcmipp_present
+	fi
 else
     is_dcmipp_present
 fi
 
 if [ "$DCMIPP_SENSOR" != "NOTFOUND" ]; then
     if [ "$DCMIPP_SENSOR" = "gc2145" ]; then
-        sensorbuscode="RGB565_2X8_BE"
+        sensorbuscode_constrain="BE"
+        parallelbuscode="RGB565_2X8_BE"
     elif [ "$DCMIPP_SENSOR" = "ov5640" ]; then
-        sensorbuscode="RGB565_2X8_LE"
+        sensorbuscode_constrain="LE"
+        parallelbuscode="RGB565_2X8_LE"
     fi
+    sensordev=$(media-ctl -d $mediadev -p -e "$sensorsubdev" | grep "node name" | awk -F\name '{print $2}')
+    sensorbuscode=`v4l2-ctl --list-subdev-mbus-codes -d $sensordev | grep RGB565 | grep "$sensorbuscode_constrain" | awk -FMEDIA_BUS_FMT_ '{print $2}'| head -n 1`
+    echo "sensor mbus-code: "$sensorbuscode
     media-ctl -d $mediadev --set-v4l2 "'$sensorsubdev':0[fmt:$sensorbuscode/${CAMERA_WIDTH}x${CAMERA_HEIGHT}@1/${FPS} field:none]"
     media-ctl -d $mediadev --set-v4l2 "'$bridgesubdev':2[fmt:$sensorbuscode/${CAMERA_WIDTH}x${CAMERA_HEIGHT}]"
     media-ctl -d $mediadev --set-v4l2 "'$interfacesubdev':1[fmt:RGB565_2X8_LE/${CAMERA_WIDTH}x${CAMERA_HEIGHT}]"
     media-ctl -d $mediadev --set-v4l2 "'dcmipp_dump_postproc':1[fmt:RGB565_2X8_LE/${CAMERA_WIDTH}x${CAMERA_HEIGHT}]"
     media-ctl -d $mediadev --set-v4l2 "'dcmipp_dump_postproc':0[compose: (0,0)/${WIDTH}x${HEIGHT}]"
-    v4l2-ctl -d /dev/v4l-subdev6 --set-ctrl=horizontal_flip=1
-    V4L2_CAPS="video/x-raw,format=RGB16"
+    V4L2_CAPS="video/x-raw,format=RGB16,width=$WIDTH, height=$HEIGHT"
     V4L_OPT=""
 else
     get_webcam_device
