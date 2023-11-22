b46991fe85dde02d49d9df714ff96fe4925c646f
Maxence GUILHIN <maxence.guilhin@st.com>
2023-11-22 18:05:26 +0100

    cv-apps : fix label file / resources path issues
    Signed-off-by: Maxence GUILHIN <maxence.guilhin@st.com>

--- recipes-samples/image-classification/files/nbg/launch_bin_image_classification_testdata.sh
+++ recipes-samples/image-classification/files/nbg/launch_bin_image_classification_testdata.sh
@@ -2,7 +2,7 @@
 weston_user=$(ps aux | grep '/usr/bin/weston '|grep -v 'grep'|awk '{print $1}')
 
 source /usr/local/demo-ai/resources/config_board.sh
-cmd="/usr/local/demo-ai/image-classification/nbg/nbg_image_classification  -m /usr/local/demo-ai/image-classification/models/mobilenet/$IMAGE_CLASSIFICATION_MODEL.nb -l /usr/local/demo-ai/image-classification/models/mobilenet/$IMAGE_CLASSIFICATION_LABEL_nbg.txt -i /usr/local/demo-ai/image-classification/models/mobilenet/testdata/"
+cmd="/usr/local/demo-ai/image-classification/nbg/nbg_image_classification  -m /usr/local/demo-ai/image-classification/models/mobilenet/$IMAGE_CLASSIFICATION_MODEL.nb -l /usr/local/demo-ai/image-classification/models/mobilenet/$IMAGE_CLASSIFICATION_LABEL\_nbg.txt -i /usr/local/demo-ai/image-classification/models/mobilenet/testdata/"
 
 if [ "$weston_user" != "root" ]; then
 	echo "user : "$weston_user
